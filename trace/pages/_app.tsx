ff788b89921dbee8d8012cb841aefd1813300e05
이희제 <holim1226@gmail.com>
2020-12-20 02:30:17 +0900

    Feat : 헤더 기능 구축

--- trace/pages/_app.tsx
+++ trace/pages/_app.tsx
@@ -1,9 +1,16 @@
 import wrapper from "../store/configureStore";
 import withReduxSaga from "next-redux-saga";
 import { AppProps } from "next/dist/next-server/lib/router/router";
+import Header from "components/layouts/Header";
+import "styles/global.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <Header />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
 // 리덕스로 쓰겠다는 뜻.
