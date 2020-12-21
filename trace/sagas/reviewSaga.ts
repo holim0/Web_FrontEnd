77621855ceadae45478000652f7a19bf8ae0e5f0
이희제 <holim1226@gmail.com>
2020-12-22 02:09:58 +0900

    Chore: 유저 리덕스, 로그인 사가 파일 생성

--- trace/sagas/reviewSaga.ts
+++ trace/sagas/reviewSaga.ts
@@ -1,6 +1,14 @@
 import { all, fork, takeLatest } from "redux-saga/effects";
 import { reviewWriteSubmit } from "redux/review";
+import axios from "axios";
 
+// 글쓰기 업로드 서버에 요청
+
+function writePost({ data }) {
+    return axios.post("url", data);
+}
+
+// 글쓰기 업데이트 상황 지켜보는 것.
 function* writeSubmit() {
     yield takeLatest(reviewWriteSubmit, () => console.log("saga"));
 }
