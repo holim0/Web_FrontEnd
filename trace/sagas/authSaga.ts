be4c2f06bd163bf5d134a03591aa750bcd4e4fcc
이희제 <holim1226@gmail.com>
2021-01-12 22:09:38 +0900

    Feat : 새로고침 유지 기능 추가

--- trace/sagas/authSaga.ts
+++ trace/sagas/authSaga.ts
@@ -1,6 +1,9 @@
+//새로고침시 accessToken 다시 가져오기
 import { all, fork, takeLatest, put, call } from "redux-saga/effects";
 import axios from "axios";
 
+import { setAccessToken } from "Redux/user";
+import { loginSuccess } from "Redux/login";
 import {
     getAuthbyTokenReq,
     getAuthbyTokenReqSuccess,
@@ -17,7 +20,15 @@ function getAuthbyToken() {
 function* AuthSagaReq() {
     try {
         const res = yield call(getAuthbyToken);
-        console.log(res);
+
+        if (res.data.success) {
+            const Token = res.data.data.accessToken;
+            yield put(setAccessToken(Token));
+            yield put(loginSuccess());
+            axios.defaults.headers.common["Authorization"] = `Bearer ${Token}`;
+        } else {
+            alert("오류 발생!");
+        }
     } catch (error) {
         console.log(error);
     }
