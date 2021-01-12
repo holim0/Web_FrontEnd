f9164e3d80fc854ae54ad8dd45ea36945e8cb89f
이희제 <holim1226@gmail.com>
2021-01-12 22:23:42 +0900

    Fix : meta 태그 제거

--- trace/pages/_document.tsx
+++ trace/pages/_document.tsx
@@ -4,12 +4,7 @@ export default class MyDocument extends Document {
     render() {
         return (
             <Html>
-                <Head>
-                    <meta
-                        http-equiv="Content-Security-Policy"
-                        content="upgrade-insecure-requests"
-                    />
-                </Head>
+                <Head></Head>
                 <body>
                     <Main />
                     <NextScript />
